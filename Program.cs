2c8c75c5dfe19f89bf91e31c6836bc5c70a10c76
kienhao127 <luongkienhao@gmail.com>
2018-08-05 13:51:21 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprioriAlgorithm
+{
+    public class item
+    {
+        public List<string> itemset = new List<string>();
+        public int count = 0;
+    }
+    class Program
+    {
+        public static List<string> loadCsvFile(string filePath)
+        {
+            var reader = new StreamReader(File.OpenRead(filePath));
+            List<string> lines = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                lines.Add(line);
+            }
+            return lines;
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> foodMart = loadCsvFile("./../../FoodMart.csv");
+        }
+    }
+}
